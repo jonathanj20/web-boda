c37c98785cb743df907288bb9e81d150485359f3
jonathanj20 <jonathanpossada@gmail.com>
2024-03-05 21:28:13 -0700

    Se optimizó el código de la galería

--- app/components/Gallery.jsx
+++ app/components/Gallery.jsx
@@ -6,14 +6,12 @@ import 'swiper/css';
 export default function Gallery() {
     const [imageFullVisible, setImageFullVisible] = useState(false);
     const classFullImage = `${imageFullVisible ? '' : 'hidden'}`;
-    const [image, setImage] = useState('');
-    const [images, setImages] = useState(['/fondo_8.jpg', '/fondo_10.png', '/fondo_11.png', '/fondo2.jpeg', '/fondo4.jpeg', '/novios_prueba.png',
+    const [images] = useState(['/fondo_8.jpg', '/fondo_10.png', '/fondo_11.png', '/fondo2.jpeg', '/fondo4.jpeg', '/novios_prueba.png',
         '/background1.jpg', '/background2.jpg']);
     const [indexImage, setIndexImage] = useState(0);
     const [swiper, setSwiper] = useState(null); // Agrega el estado para el objeto swiper
-    const [newIndex, setNewIndex] = useState(0);
 
-    /*useEffect es un componente que recibe como parámetros una función callback y las dependencias.
+    /*useEffect es un hook que recibe como parámetros una función callback y las dependencias.
     Y ejecuta el código que está dentro de la función cada vez que se renderiza el componente (es decir,
     cada vez que el componente muestre elementos en el DOM)
     
@@ -31,9 +29,8 @@ export default function Gallery() {
 
     }, [indexImage, swiper]);
 
-    function toggleFullImage(src, index) {
+    function toggleFullImage(index) {
         setImageFullVisible(!imageFullVisible);
-        setImage(src);
         setIndexImage(index);
     }
 
@@ -53,27 +50,19 @@ export default function Gallery() {
         }
     }
 
-    function handleIndexSwiper(swiper) {
-        /**Esta función se ejecuta cada vez que se cambia de slide, y recibe como parámetro el objeto
-         * swiper, del cual accedemos al indice activo de cada slide y se lo vamos poniendo a la variable
-         * newIndex
-         */
-        setNewIndex(swiper.activeIndex);
-    }
-
     return (
         <>
             <section className="bg-[url('../public/background1.jpg')] bg-cover bg-no-repeat p-5 mt-5" id="gallery">
                 <h2 className="text-center my-5">Galería de fotos</h2>
                 <div className="space-y-7 flex items-center flex-col bg-white py-10 md:space-y-0 md:grid md:place-items-center md:grid-cols-4 md:p-10 md:gap-5">
-                    <img src="/fondo_8.jpg" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage('/fondo_8.jpg', 0)} />
-                    <img src="/fondo_10.png" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage('/fondo_10.png', 1)} />
-                    <img src="/fondo_11.png" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage('/fondo_11.png', 2)} />
-                    <img src="/fondo2.jpeg" alt="" className="w-9/12  md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage('/fondo2.jpeg', 3)} />
-                    <img src="/fondo4.jpeg" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage('/fondo4.jpeg', 4)} />
-                    <img src="/novios_prueba.png" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage('/novios_prueba.png', 5)} />
-                    <img src="/background1.jpg" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage('/background1.jpg', 6)} />
-                    <img src="/background2.jpg" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage('/background2.jpg', 7)} />
+                    <img src="/fondo_8.jpg" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage(0)} />
+                    <img src="/fondo_10.png" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage(1)} />
+                    <img src="/fondo_11.png" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage(2)} />
+                    <img src="/fondo2.jpeg" alt="" className="w-9/12  md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage(3)} />
+                    <img src="/fondo4.jpeg" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage(4)} />
+                    <img src="/novios_prueba.png" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage(5)} />
+                    <img src="/background1.jpg" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage(6)} />
+                    <img src="/background2.jpg" alt="" className="w-9/12 md:w-72 md:h-72 border-4 border-yellow-500 transition-all ease-in-out delay-75 hover:scale-110 cursor-pointer" onClick={() => toggleFullImage(7)} />
                 </ div>
             </section >
             <div className={`${classFullImage} bg-black bg-opacity-30 p-5 fixed left-0 right-0 bottom-0 top-0 mx-auto`}>
@@ -102,56 +91,18 @@ export default function Gallery() {
                         slidesPerView={1}
                         initialSlide={indexImage}
                         onSwiper={(swiper) => setSwiper(swiper)}
-                        onSlideChange={handleIndexSwiper}
                     >
-                        <SwiperSlide>
-                            <img src="/fondo_8.jpg" alt="" className="md:mx-auto" />
-                            <div>
-                                <p className="text-center text-white font-bold">Imagen {newIndex + 1} de 8</p>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/fondo_10.png" alt="" className="md:mx-auto" />
-                            <div>
-                                <p className="text-center text-white font-bold">Imagen {newIndex + 1} de 8</p>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/fondo_11.png" alt="" className="md:mx-auto" />
-                            <div>
-                                <p className="text-center text-white font-bold">Imagen {newIndex + 1} de 8</p>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/fondo2.jpeg" alt="" className="md:mx-auto" />
-                            <div>
-                                <p className="text-center text-white font-bold">Imagen {newIndex + 1} de 8</p>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/fondo4.jpeg" alt="" className="md:mx-auto" />
-                            <div>
-                                <p className="text-center text-white font-bold">Imagen {newIndex + 1} de 8</p>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/novios_prueba.png" alt="" className="md:mx-auto" />
-                            <div>
-                                <p className="text-center text-white font-bold">Imagen {newIndex + 1} de 8</p>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/background1.jpg" alt="" className="md:mx-auto" />
-                            <div>
-                                <p className="text-center text-white font-bold">Imagen {newIndex + 1} de 8</p>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src="/background2.jpg" alt="" className="md:mx-auto" />
-                            <div>
-                                <p className="text-center text-white font-bold">Imagen {newIndex + 1} de 8</p>
-                            </div>
-                        </SwiperSlide>
+
+                        {
+                            /**El map me permite navegar entre la galería de imagenes de forma dinámica */
+                            images.map((img, index) => (
+                                <SwiperSlide key={index}>
+                                    <img src={img} className="md:mx-auto md:h-96 md:w-96" alt={`Slide ${index}`} />
+                                    <div>
+                                        <p className="text-center text-white font-bold">Imagen {index + 1} de {images.length}</p>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
                     </Swiper>
                 </div>
             </div>
